ac4ac8e5ea992a7f3e8356fa9f7c3127833833bb
NikhilReddy-hub <alimili.nikhil.s70@kalvium.community>
2025-01-21 11:59:13 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -34,17 +34,36 @@
 
 const express = require('express');
 const { resolve } = require('path');
-
+const students = require('./data.json')
 const app = express();
 const port = 3010;
-
+app.use(express.json())
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+app.post('/students/above-threshold' ,(req,res) => {
+  const {threshold} = req.body;
 
+  if(typeof threshold !== 'number'){
+    return res.status(400).json({error : 'Invalid threshold value.'})
+  }
+  const filterData = students.filter(student => student.total > threshold)
+
+  const Response = {
+    count : filterData.length ,
+    students : filterData.map(student =>({
+      name : student.name,
+      total:student.total
+    }))
+  }
+  res.status(200).json(Response)
+
+
+})
 app.listen(port, () => {
+  console.log(students)
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
